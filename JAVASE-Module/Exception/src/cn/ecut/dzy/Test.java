6d57be1aa81b8d493b3786b42a226c540cefdd0c
dingzhenyun <2901082882@qq.com>
2020-11-27 08:46:53 +0800

    另外一台电脑进行1测试

---
+++ JAVASE-Module/Exception/src/cn/ecut/dzy/Test.java
@@ -0,0 +1,14 @@
+package cn.ecut.dzy;/**
+ * @author DingZhenYun
+ * @create 2020-11-27 上午 8:45
+ */
+
+/**
+ * @program: JavaDemo->Test
+ * @description:
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("sssssss");
+    }
+}
