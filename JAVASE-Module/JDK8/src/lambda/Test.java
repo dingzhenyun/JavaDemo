83db992733cdd41282145873c7bdce7ff898977d
dingzhenyun <2901082882@qq.com>
2020-12-16 16:03:02 +0800

    test

---
+++ JAVASE-Module/JDK8/src/lambda/Test.java
@@ -0,0 +1,14 @@
+package lambda;/**
+ * @author DingZhenYun
+ * @create 2020-12-16 下午 16:02
+ */
+
+/**
+ * @program: JavaDemo->Test
+ * @description:
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("dd");
+    }
+}
